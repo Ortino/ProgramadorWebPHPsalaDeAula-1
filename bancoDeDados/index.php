8c7f9b8fa2195d0a64bcabde956dcc705ce1b81c
MARCIO DE OLIVEIRA VELASCO <prof.marciovelasco@gmail.com>
2022-10-28 21:39:54 -0300

    Banco de Dados
    Conexão com o Banco de Dados e Aula de SQL

--- bancoDeDados/index.php
+++ bancoDeDados/index.php
@@ -1,4 +1,44 @@
 <!DOCTYPE html>
+<?php
+    require_once "conexao.php";
+
+    if(isset($_POST["email"]) || isset($_POST["senha"])){
+        if(strlen($_POST["email"]) == 0){
+            echo "Preencha seu e-mail!";
+        } else if(strlen($_POST["senha"]) == 0){
+            echo "Preencha a sua senha!";
+        } else {
+            //real_escape_string() = função que retorna uma string sem caractere especiais...
+            $email = $conexao->real_escape_string($_POST["email"]);
+            $senha = $conexao->real_escape_string($_POST["senha"]);
+
+            $sql_code = "select *  from usuario where login = '$email' and senha = '$senha'";
+            $sql_query = $conexao->query($sql_code) or die("Falha na execução do código SQL: " . $conexao->error);
+            
+            //Quantidade de linhas retornado;
+            $qtd = $sql_query->num_rows; //Atributo num_rows = número de linhas
+            if($qtd == 1){
+                // array ([id]=>1, [nome]=> "Ususário de Teste", [login] => "teste@gmail.com", [senha]=> "teste123")
+                $usuario = $sql_query->fetch_assoc();
+
+                if(!isset($_SESSION)){
+                    session_start();
+                }
+
+                // $_SESSION['user'] = $usuario;
+                $_SESSION['id'] = $usuario["id"];
+                $_SESSION['nome'] = $usuario["nome"];
+
+                //Redirecionando para a página
+                header("Location: login.php");
+            } else {
+                echo "Falha ao logar! E-mail ou senha incorretos";
+            }
+
+        }
+
+    }
+?>
 <html lang="pt-BR">
 
 <head>
@@ -22,12 +62,12 @@
 
         </section>
         
-        <form action="" method="post" name="f" onsubmit="return validar()">
+        <form action="" method="post"  >
             <br>
             
             <label class="form-label">E-mail:</label>
             <input type="email" name="email" class="form-control">
-            
+            <br>
             <label class="form-label">Senha:</label>
             <input type="password" name="senha" class="form-control">
             <br>
